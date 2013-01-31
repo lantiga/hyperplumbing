8a018f8d68edae9d04938839de4b5cf1ba02a7ad
Luca Antiga <luca.antiga@orobix.com>
2013-01-31 12:12:23 +0100

    Added fnk groups.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [de.kotka/lazymap "3.1.0"]
                  [exoref "0.1.3"]
                  [prismatic/plumbing "0.0.2-SNAPSHOT"]])
--- src/hyperplumbing/graph.clj
+++ src/hyperplumbing/graph.clj
@@ -1,15 +1,24 @@
 (ns hyperplumbing.graph
   (:use plumbing.graph
         [exoref.delay :only [exodelay]])
-  (:require [plumbing.lazymap :as lazymap]))
+  (:require [lazymap.core :as lazymap]))
 
 
 (defn distributed-compile
-  [g]
-  (abstract-compile
-   g
-   (fn [m] (into (lazymap/lazy-hash-map) m))
-   (fn [m k f] (lazymap/delay-assoc m k (exodelay (name k) (f))))))
-
-;; FIXME: right now the Redis key is set equal to k, which is not
-;; correct. It should become the full keypath.
+  ([g & groups]
+     (abstract-compile
+      g
+      (fn [m] (with-meta (into (lazymap/lazy-hash-map) m) (meta m)))
+      (fn [m k f]
+        (let [;; in the following two lines we need k to be the key path, not just the key
+              ;; TODO: modify abstract-compile in plumbing.graph
+              fnk-meta (meta (get-in g [k]))
+              redis-key (name (or (:redis-key fnk-meta) k))
+              redis-key (if-let [redis-key-prefix (:redis-key-prefix (meta m))]
+                          (str redis-key-prefix ":" redis-key)
+                          redis-key)]
+          (lazymap/delay-assoc
+           m k
+           (if (or (nil? groups) ((set groups) (:group fnk-meta)))
+             (exodelay redis-key (f))
+             (exodelay redis-key))))))))
